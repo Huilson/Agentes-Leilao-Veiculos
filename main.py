0ea017dd19e58f17b871987bc7523ce0af4c7308
Huilson J. Lorenzi <hill.winterclaw@gmail.com>
2025-10-20 18:49:29 -0300

    Preço e Quantidade

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ class AgenteVendedor(Agent):
     @pl(gain, Goal("vender"), Belief("negociar"))
     def iniciarLeilao(self, src):
         self.print("Vou vender um carro")
-        self.send("AgenteComprador", tell, Belief("temCarro"), "S2B")
+        self.send("AgenteComprador", tell, Belief("temCarro",5), "S2B")
 
-    @pl(gain, Goal("negociar"))
-    def venderCarro(self, src):
-        self.print(f"{src} quer comprar meu carro... OK!")
+    @pl(gain, Goal("negociar", Any))
+    def venderCarro(self, src, negociar):
+        self.print(f"{src} quer comprar meu carro... OK! Valor: R${negociar}")
         self.stop_cycle()
 
 
@@ -22,11 +22,12 @@ class AgenteComprador(Agent):
     def __init__(self, agt_name):
         super().__init__(agt_name)
 
-    @pl(gain, Belief("temCarro"))
-    def comprarCarro(self, src):
-        self.print(f"O {src} tem um carro disponivel para que eu possa comprar")
-        self.send("AgenteVendedor", achieve, Goal("negociar"), "S2B")
+    @pl(gain, Belief("temCarro", Any))
+    def comprarCarro(self, src, temCarro):
+        self.print(f"O {src} tem um carro disponivel para que eu possa comprar, Quantidade: {temCarro}")
+        self.send("AgenteVendedor", achieve, Goal("negociar", 50000), "S2B")
         self.stop_cycle()
+
 
 comprador = AgenteComprador("AgenteComprador")
 vendedor = AgenteVendedor("AgenteVendedor")
