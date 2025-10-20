ab07f1e65d1b5666778454eb4062a26281f250cd
Huilson J. Lorenzi <hill.winterclaw@gmail.com>
2025-10-20 18:24:27 -0300

    agentes
    Adicionados agentes de compra e venda

--- main.py
+++ main.py
@@ -1,83 +1,37 @@
 from maspy import *
-import random
 
-class MonitoramentoUrbano(Environment):
-    def __init__(self, env_name):
-        super().__init__(env_name)
-        self.create(Percept("tem_obstaculo"))
-
-    def desviar_obstaculo(self, src):
-        desvio = random.choice([True, False])
-        print(desvio)
-        if desvio:
-            self.print("Obstáculo desviado!")
-            self.create(Percept("obstaculo_resolvido"))
-        else:
-            self.print("Obstáculo não desviado!")
-            self.create(Percept("obstaculo_nao_resolvido"))
-
-
-class VA(Agent):
+class AgenteVendedor(Agent):
     def __init__(self, agt_name):
         super().__init__(agt_name)
-        self.add(Belief("conduzindoVA"))
+        self.add(Belief("negociar"))
+        self.add(Goal("vender"))
 
-    @pl(gain, Belief("conduzindoVA"))
-    def verifica_via_urbana(self, src):
-        self.print("Verificar via urbana")
-        percepcao1 = self.get(Belief("tem_obstaculo", source="BR101"))
-        self.print(percepcao1.key)
-        if percepcao1:
-            self.desviar_obstaculo()
-            self.perceive("BR101")
-            self.add(Goal("verificar_acao"))
+    @pl(gain, Goal("vender"), Belief("negociar"))
+    def venderCarro(self, src):
+        self.print("Vou vender um carro")
+        self.send("AgenteComprador", tell, Belief("temCarro"), "S2B")
 
-    @pl(gain, Goal("verificar_acao"), Belief("conduzindoVA"))
-    def verificar_acao(self, src):
-        percepcao2 = self.get(Belief("obstaculo_resolvido", source="BR101"))
-        percepcao3 = self.get(Belief("obstaculo_nao_resolvido", source="BR101"))
-        if percepcao2:
-            self.print("Desvio realizado com sucesso!")
-            self.send("ControladorRemoto", tell, Belief("VA_operacional"))
-            self.stop_cycle()
-        elif percepcao3:
-            self.add(Belief("obstaculo"))
-            self.add(Goal("manobraCritica"))
-        else:
-            self.add(Goal("verificar_acao"))
-
-    @pl(gain, Goal("manobraCritica"), Belief("obstaculo"))
-    def avisar_stakeholder(self, src):
-        # envia msg para o agente Stakeholder
-        self.print("Manobra crítica; Acionar stakeholder")
-        self.send("ControladorRemoto", tell, Belief("VA_precisa_ajuda"), "V2C")
-
-    @pl(gain, Goal("manobraEmergencia"))
-    def executar_manobra(self, src):
-        self.print(f"Manobra executada conforme stakeholder: {src}")
+    @pl(gain, Goal("negociar"))
+    def venderCarro(self, src):
+        self.print(f"{src} quer comprar meu carro... OK!")
         self.stop_cycle()
 
 
-class Stakeholder(Agent):
+class AgenteComprador(Agent):
     def __init__(self, agt_name):
         super().__init__(agt_name)
 
-    @pl(gain, Belief("VA_precisa_ajuda"))
-    def enviar_manobra(self, src):
-        # envia msg para agente Waymo (VA)
-        self.send("Waymo", achieve, Goal("manobraEmergencia"), "V2C")
-        self.stop_cycle()
-
-    @pl(gain, Belief("VA_operacional"))
-    def desligar(self, src):
+    @pl(gain, Belief("temCarro"))
+    def comprarCarro(self, src):
+        self.print(f"O {src} tem um carro disponivel para que eu possa comprar")
+        self.send("AgenteVendedor", achieve, Goal("negociar"), "S2B")
         self.stop_cycle()
 
 
-if __name__ == "__main__":
-    monitor = MonitoramentoUrbano("BR101")
-    veiculo = VA("Waymo")
-    controlador = Stakeholder("ControladorRemoto")
-    comunicacao_ch = Channel("V2C")
-    Admin().connect_to([veiculo, controlador], comunicacao_ch)
+if __name__ == "main":
+    comprador = AgenteComprador("AgenteComprador")
+    vendedor = AgenteVendedor("AgenteVendedor")
+    canal = Channel("S2B")
+    Admin().connect_to([comprador, vendedor], canal)
     Admin().report = True
     Admin().start_system()
